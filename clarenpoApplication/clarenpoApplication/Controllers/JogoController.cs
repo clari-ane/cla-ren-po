44125db30f93626238a7ec3750b2ae026b1a422f
Clariane Menezes <csmenezeess@gmail.com>
2019-06-29 23:28:31 -0300

    inicio

---
+++ clarenpoApplication/clarenpoApplication/Controllers/JogoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace clarenpoApplication.Controllers
+{
+    public class JogoController : Controller
+    {
+        // GET: Jogo
+        public ActionResult Regras()
+        {
+            return View();
+        }
+
+        public ActionResult Torneio()
+        {
+            return View();
+        }
+    }
+}
