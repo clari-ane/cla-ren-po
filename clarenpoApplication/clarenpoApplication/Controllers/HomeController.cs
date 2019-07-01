89b5694d39d0d5cca7a9e04cbbf83fdb63dade4c
Clariane Menezes <csmenezeess@gmail.com>
2019-07-01 00:40:30 -0300

    .

--- clarenpoApplication/clarenpoApplication/Controllers/HomeController.cs
+++ clarenpoApplication/clarenpoApplication/Controllers/HomeController.cs
@@ -12,24 +12,5 @@ namespace clarenpoApplication.Controllers
         {
             return View();
         }
-
-        public ActionResult Regras()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
--- clarenpoApplication/clarenpoApplication/Controllers/JogoController.cs
+++ clarenpoApplication/clarenpoApplication/Controllers/JogoController.cs
@@ -14,6 +14,11 @@ namespace clarenpoApplication.Controllers
             return View();
         }
 
+        public ActionResult OJogo()
+        {
+            return View();
+        }
+
         public ActionResult Torneio()
         {
             return View();
